a04dc70b2efa000ce2033d802ab92759c7bc0e89
Kristiyan Petrov <petrov.kris03@gmail.com>
2022-03-09 11:53:30 +0200

    Addded join form that redirects to a game

--- RUMEN/frontend/src/components/JoinGame.js
+++ RUMEN/frontend/src/components/JoinGame.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import {Button, Col, Container, FormControl, Row} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Button, Col, Container, FloatingLabel, Form, FormControl, Row} from "react-bootstrap";
 import {AiOutlineRollback} from "react-icons/ai";
 import Footer from "./Footer";
+import {useNavigate} from 'react-router-dom';
 
 export default function JoinGame() {
+    let navigate = useNavigate();
     const goHome = () => {
-        window.location.href = '/home'
+        navigate('/home');
     }
+
+    const joinGame = () => {
+        if (code !== '') {
+            navigate('/join/' + code);
+        }
+    }
+
+    const [code, setCode] = useState('');
 
     return (
         <>
@@ -17,16 +27,22 @@ export default function JoinGame() {
                         <h1 className={'text-warning'}>Join a Game</h1>
                     </Col>
                 </Row>
+                <Form onSubmit={joinGame} >
                 <Row className="justify-content-center text-center mt-5">
                     <Col className={'col-sm-5'}>
-                        <FormControl className={'text-center'} size={'lg'} type={'text'} placeholder={'Input your game code'}/>
+                            <FloatingLabel label={'Input your game code'}>
+                                <FormControl onChange={(e) => {
+                                    setCode(e.target.value)
+                                }} placeholder={''} size={'lg'} type={'text'}/>
+                            </FloatingLabel>
                     </Col>
                 </Row>
                 <Row className={'mt-3 justify-content-center text-center'}>
                     <Col>
-                        <Button size={'lg'} variant={'dark'}>Submit</Button>
+                        <Button size={'lg'} variant={'dark'} type={'submit'}>Submit</Button>
                     </Col>
                 </Row>
+                </Form>
             </Container>
             <Footer/>
         </>
